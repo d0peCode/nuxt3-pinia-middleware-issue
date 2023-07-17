a8683e2962d23d22765e9ab2d7320576418bbc24
Borys Tyminski <tyminski.borys@gmail.com>
2023-07-17 07:23:00 +0200

    remove button

--- app.vue
+++ app.vue
@@ -1,11 +1,3 @@
 <template>
-  <pre>token: {{ store.token }}</pre>
-  <button @click="logout">logout</button>
+  <div>hello world</div>
 </template>
-<script setup lang="ts">
-import {useStore} from "~/store/useStore";
-const store = useStore()
-const logout = () => {
-  navigateTo('/')
-}
-</script>

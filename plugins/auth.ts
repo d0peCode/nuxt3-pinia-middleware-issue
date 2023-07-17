ccc34b197a1650a405777a41d761ad9fc1e2619f
Borys Tyminski <tyminski.borys@gmail.com>
2023-07-17 07:55:03 +0200

    change middleware to plugin

--- plugins/auth.ts
+++ plugins/auth.ts
@@ -1,4 +1,4 @@
-export default defineNuxtRouteMiddleware(async () => {
+export default defineNuxtPlugin(async () => {
     await useFetch('/api/setCookie')
     await useFetch('/api/getCookie')
 });
